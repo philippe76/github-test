50e9fd3561b87bf4c06ffe0461ca3bb59ea82391
Philippe Lanougadere <soufiane92230@gmail.com>
2020-12-05 17:47:06 +0100

    Test live changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing React deployment to Github pages
+          Testing React Live chanegs
         </p>
         <a
           className="App-link"
