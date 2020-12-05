ffed78b01f5a7a0024c132475fee3179c93c1447
Philippe Lanougadere <soufiane92230@gmail.com>
2020-12-05 17:41:45 +0100

    Change title React page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Testing React deployment to Github pages
         </p>
         <a
           className="App-link"
